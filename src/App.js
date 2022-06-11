70c0e781c0efe1daf835c54b5f0669edbee36729
Mario Maestro del Caz <mmaestro@encamina.com>
2022-06-11 13:17:17 +0200

    Componente renombrado +Añadidos estilos a la pagina principal

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import './App.css';
+import './App.scss';
 import React,{useState} from 'react';
 import Categories from './components/categories/Categories.js';
 import Breeds from './components/breeds/breeds';
+import MenuOption from './components/menuOption/menuOption.js';
 
 function App() {
   const [categories, setCategories] = useState([]);
   const [breeds, setBreeds] = useState([]);
   return (
-    <div className="App">
-      <section className='App-content'>
+    <><div className="App">
+      {/* <section className='App-content'>
         <Categories
-        categories={categories}
-        setCategories={setCategories}>
+          categories={categories}
+          setCategories={setCategories}>
         </Categories>
       </section>
-   
+
+
+      <section className='App-content'>
+        <Breeds
+          breeds={breeds}
+          setBreeds={setBreeds}>
+        </Breeds>
+      </section> */}
+      <div className='title'>
+      <span>CATFY</span>
+      </div>
+      
+      <div className='catImage'>
+      <img src={"https://static.vecteezy.com/system/resources/previews/001/199/162/non_2x/silhouettes-cat-png.png"}></img>
+      </div>
+      
+    </div><section>
+        <MenuOption></MenuOption>
+      </section></>
     
-    <section className='App-content'>
-      <Breeds
-      breeds={breeds}
-      setBreeds={setBreeds}>
-      </Breeds>
-    </section>
-    </div>
   );
 }
 
