91afe7564db0911354bd367f1b98fbd271839e37
Mario Maestro del Caz <mmaestro@encamina.com>
2022-06-13 13:07:12 +0200

    +Añadido mostrar mas de una imagen

--- src/services/getCategoriesById.js
+++ src/services/getCategoriesById.js
@@ -1,7 +1,15 @@
 const apiKey = "6baccb43-514e-41e0-a52c-c7ada0ac6bac";
 
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
 export default function getCategoriesById({keyword}){
-    const url = `https://api.thecatapi.com/v1/images/search?category_ids=${keyword}`
-    return fetch(url)
+    const url = `https://api.thecatapi.com/v1/images/search?category_ids=${keyword}&limit=10`
+    return fetch(url,requestOptions)
     .then(res => res.json())
 }
